e4133574162fdef13a7e6f325a6fb368f4bdba05
Calvin_Mazhindu <88860341+cavcom2010@users.noreply.github.com>
2026-06-15 13:40:18 +0100

    Test person name editing

--- apps/people/tests/test_views.py
+++ apps/people/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from apps.families.models import Family, FamilyMembership
 from apps.people.models import Person
@@ -39,3 +40,85 @@ class PersonViewTests(TestCase):
         response = self.client.get(f"/people/{other_person.id}/drawer/")
 
         self.assertEqual(response.status_code, 404)
+
+    def test_current_user_can_edit_their_tree_name(self):
+        family = Family.objects.create(name="Test Family", slug="test-family")
+        person = Person.objects.create(
+            family=family,
+            first_name="Auto",
+            last_name="Generated",
+            created_by=self.user,
+        )
+        FamilyMembership.objects.create(
+            family=family,
+            user=self.user,
+            person=person,
+            role=FamilyMembership.Role.OWNER,
+        )
+
+        response = self.client.post(
+            reverse("person_edit_name", args=[person.id]),
+            {
+                "first_name": "Calvin",
+                "middle_name": "",
+                "last_name": "Mazhindu",
+                "maiden_name": "",
+            },
+        )
+
+        self.assertRedirects(response, reverse("tree"))
+        person.refresh_from_db()
+        self.assertEqual(person.first_name, "Calvin")
+        self.assertEqual(person.last_name, "Mazhindu")
+
+    def test_person_name_edit_form_is_scoped_to_current_family(self):
+        primary_family = Family.objects.create(name="Primary Family", slug="primary")
+        FamilyMembership.objects.create(family=primary_family, user=self.user)
+        other_family = Family.objects.create(name="Other Family", slug="other")
+        other_person = Person.objects.create(
+            family=other_family,
+            first_name="Hidden",
+            last_name="Person",
+            created_by=self.user,
+        )
+
+        response = self.client.get(reverse("person_edit_name", args=[other_person.id]))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_unclaimed_person_can_be_edited_by_family_owner(self):
+        family = Family.objects.create(name="Owner Family", slug="owner-family")
+        owner_person = Person.objects.create(
+            family=family,
+            first_name="Owner",
+            last_name="Person",
+            created_by=self.user,
+        )
+        editable_person = Person.objects.create(
+            family=family,
+            first_name="Wrong",
+            last_name="Name",
+            created_by=self.user,
+        )
+        FamilyMembership.objects.create(
+            family=family,
+            user=self.user,
+            person=owner_person,
+            role=FamilyMembership.Role.OWNER,
+        )
+
+        response = self.client.post(
+            reverse("person_edit_name", args=[editable_person.id]),
+            {
+                "first_name": "Right",
+                "middle_name": "",
+                "last_name": "Name",
+                "maiden_name": "",
+            },
+            HTTP_HX_REQUEST="true",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        editable_person.refresh_from_db()
+        self.assertEqual(editable_person.first_name, "Right")
+        self.assertContains(response, "Name updated")
